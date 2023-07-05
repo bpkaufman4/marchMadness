fa2bed4a2d4493ea01f149331107e8cd3c11fe3f
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-05 16:47:47 -0500

    'BK'

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../model');
+const { User } = require('../../models');
 
 router.get('/', (req, res) => {
     User.findAll({
