4c164410649028d1384fb20fa124a88e983d4b72
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-05 15:40:11 -0500

    'BK'

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,17 +1,23 @@
-function signupFormHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefault();
 
     const email = getobj('emailSignup').value;
     const password = getobj('passwordSignup').value;
 
-    fetch('api/users', {
+    const response = await fetch('api/users', {
         method:'post',
         body: JSON.stringify({
             email,
             password
         }),
         headers: { 'Content-Type': 'application/json'}
-    }).then((response) => {console.log(response)})
+    });
+
+    if(response.ok) {
+        console.log('success');
+    } else {
+        alert(response.statusText);
+    }
 }
 
 getobj('signupForm').addEventListener('submit', signupFormHandler);
