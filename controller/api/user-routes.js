1ac36b6716ec2f322f8cac19b733f228d1a9a833
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-05 16:16:51 -0500

    'BK'

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -118,6 +118,7 @@ router.post('/login', (req, res) => {
 
 router.post('/logout', (req, res) => {
     console.log(req);
+    return;
     if(req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
