c22847857b29205ba711f1f26a44f4e7271c3641
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-05 15:49:23 -0500

    'BK'

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const PORT = process.env.PORT || 3001;
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: process.env.SECRET;
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
 app.engine('handlebars', hbs.engine);
 
@@ -16,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(controller);
+app.use(session(sess));
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`listening on port ${PORT}`);
