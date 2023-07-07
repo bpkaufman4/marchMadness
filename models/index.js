fe37ebabcb4d6bc5a6cbc79d981a13988064460b
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-07 10:04:56 -0500

    'BK'

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
 const Reference = require('../models/Reference');
+const ReferenceSet = require('../models/ReferenceSet');
 
-module.exports = { User, Reference };
+module.exports = { User, Reference, ReferenceSet };
