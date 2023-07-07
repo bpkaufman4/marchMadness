f671e4c199136ad8c7ca3fcafb7e885c523892a3
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-07 11:22:19 -0500

    'BK'

--- models/index.js
+++ models/index.js
@@ -3,5 +3,5 @@ const Reference = require('../models/Reference');
 const ReferenceSet = require('../models/ReferenceSet');
 
 User.belongsToMany(Reference, {through: 'userStatusCd'});
-Reference.hasMany(User);
+Reference.hasMany(User, {as: 'userStatus'});
 module.exports = { User, Reference, ReferenceSet };
