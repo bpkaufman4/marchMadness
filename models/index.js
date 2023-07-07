e9ca2d38a657c804f7e765042efb6d16d3b56077
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-07 11:33:11 -0500

    'BK'

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ const User = require('../models/User');
 const Reference = require('../models/Reference');
 const ReferenceSet = require('../models/ReferenceSet');
 
-User.hasOne(Reference);
+User.hasOne(Reference, {
+    foreignKey: 'statusCd'
+});
 Reference.belongsTo(User, {foreignKey: 'statusCd'});
 module.exports = { User, Reference, ReferenceSet };
