d313a1de8a75097944f75be50a534c04c6c0e90e
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-07 10:14:10 -0500

    'BK'

--- models/ReferenceSet.js
+++ models/ReferenceSet.js
@@ -29,7 +29,7 @@ ReferenceSet.init(
         updatedAt: false,
         freezeTableName: true,
         underscored: false,
-        modelName: 'referenceSets'
+        modelName: 'referenceSet'
     }
 );
 
