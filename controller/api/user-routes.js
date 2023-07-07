a88e328c431b4feabe6586120321363fb8c30b8d
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-07 11:39:20 -0500

    'BK'

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User, Reference } = require('../../models');
-const { sequelize } = require('../../config/connection.js');
+const { sequelize } = require('sequelize');
 
 router.get('/', (req, res) => {
     User.findAll({
