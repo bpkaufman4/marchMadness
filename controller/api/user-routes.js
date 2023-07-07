09bb33abeb9bd19518d7afdaf532230a15c86408
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-07 11:41:22 -0500

    'BK'

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 router.get('/', (req, res) => {
     User.findAll({
         attributes: [
-            [Sequelize.literal('(SELECT status.display from reference status where status.referenceCd = user.statusCd)', 'statusCdDisplay')]
+            [Sequelize.literal('(SELECT status.display from reference status where status.referenceCd = user.statusCd)'), 'statusCdDisplay']
         ],
     })
     .then(dbUserData => res.json(dbUserData))
