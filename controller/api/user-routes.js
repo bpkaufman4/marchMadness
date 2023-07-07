ac5945bcdfa9de7569538304e31874fc5824be0e
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-07 13:39:13 -0500

    'BK'

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -122,6 +122,9 @@ router.post('/login', (req, res) => {
         });
 
         res.json({ user: dbUserData, message: 'Login successful' });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
     })
 });
 
