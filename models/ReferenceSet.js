74d1d353df38e5028c7039c3bfa1b887d5037f4a
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-07 10:07:58 -0500

    'BK'

--- models/ReferenceSet.js
+++ models/ReferenceSet.js
@@ -25,7 +25,6 @@ ReferenceSet.init(
         sequelize,
         timestamps: true,
         createdAt: 'created',
-        updatedAt: 'updated',
         freezeTableName: true,
         underscored: false,
         modelName: 'referenceSets'
