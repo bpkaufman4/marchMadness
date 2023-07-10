50fc2fc1db311d7ead20d451198fecd27bb90cbe
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-10 16:18:53 -0500

    'BK'

---
+++ controller/userRoutes.js
@@ -0,0 +1,25 @@
+
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { User } =  require('../models');
+
+router.get('/', (req, res) => {
+    User.findAll({
+        attributes: [
+            'fullName',
+            'email',
+            'userId'
+        ],
+        include: ['userType', 'userStatus']
+    })
+    .then(dbUserData => {
+        const users = dbUserData.map(user => user.get({ plain: true }));
+        res.render('users', { users });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+module.exports = router;
