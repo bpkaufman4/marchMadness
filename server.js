7fe68c05d5a4ffadf3e9aad4250d52447ee10aef
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-11 12:46:25 -0500

    'BK'

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(controller);
 sequelize.sync({ alter: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`listening on port ${PORT}`);
