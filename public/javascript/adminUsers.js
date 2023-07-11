61ffe0970d57f12570498efbf9d19295bb585c40
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-11 14:10:51 -0500

    'BK'

--- public/javascript/adminUsers.js
+++ public/javascript/adminUsers.js
@@ -482,7 +482,56 @@ function getUsers(){
         headers: { 'Content-Type': 'application/json' }
     }).then(response => response.json())
 	.then(data => {
-		console.log(data);
+		data.forEach(u => {
+			const row = makeElem('tr');
+			row.id=u.userId;
+			
+			const editUser = 'addUser(\'EDIT\', \''+u.userId+'\');';
+			const activeUser = 'addUser(\'ACTIVE\', \''+u.userId+'\');';
+			const deleteUser = 'addUser(\'DELETE\', \''+u.userId+'\');';
+			
+			const name = makeElem('td');
+			setColumnDisplay('fullName', name);
+			name.classList.add('fullNameHide', 'SHOWALL');
+			name.setAttribute('tabindex', 0);
+			name.setAttribute('onclick', editUser);
+			name.innerText = u.fullName;
+			row.appendChild(name);
+			
+			const email = makeElem('td');
+			setColumnDisplay('email', email);
+			email.classList.add('emailHide', 'SHOWALL');
+			email.setAttribute('onclick', editUser);
+			email.innerText = u.email;
+			row.appendChild(email);
+			
+			const userType = makeElem('td');
+			setColumnDisplay('userTypeCdMeaning', userType);
+			userType.classList.add('userTypeCdMeaningHide', 'SHOWALL')
+			userType.setAttribute('onclick', editUser);
+			userType.innerText = u.userTypeCdDisplay;
+			row.appendChild(userType);
+			
+			const userStatus = makeElem('td');
+			setColumnDisplay('statusCdMeaning', userStatus);
+			userStatus.classList.add('statusCdMeaningHide', 'SHOWALL');
+			userStatus.setAttribute('onclick', editUser);
+			userStatus.innerText = u.statusCdDisplay;
+			row.appendChild(userStatus);
+			
+			const deleteColumn = makeElem('td');
+			deleteColumn.setAttribute('tabindex', 0);
+			deleteColumn.setAttribute('onclick', (u.statusCdMeaning == 'DELETED')?activeUser:deleteUser);
+			
+			const deleteIcon = makeElem('i');
+			deleteIcon.classList.add('material-icons');
+			deleteIcon.innerText = (u.statusCdMeaning == 'DELETED')?'undo':'delete';
+			
+			deleteColumn.appendChild(deleteIcon);
+			row.appendChild(deleteColumn);
+			
+			usersTable.appendChild(row);
+		});
 	});
 }
 
