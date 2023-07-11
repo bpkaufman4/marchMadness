72108d7caecc6855fa7cd8dbd8e822fa9a531074
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-11 12:40:46 -0500

    'BK'

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.engine('handlebars', hbs.engine);
 
 app.set('view engine', 'handlebars');
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(controller);
-sequelize.sync({ alter: false }).then(() => {
+sequelize.sync({ alter: true }).then(() => {
     app.listen(PORT, () => {
         console.log(`listening on port ${PORT}`);
     });
-})
-app.use(express.static(path.join(__dirname, 'public')));
+});
