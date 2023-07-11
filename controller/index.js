5d13af2c22ba4b269a567015e88a6f4d66505331
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-11 12:38:28 -0500

    'BK'

--- controller/index.js
+++ controller/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes.js');
+const homeRoutes = require('../public');
 const userRoutes = require('./userRoutes.js');
 
 router.use('/api', apiRoutes);
