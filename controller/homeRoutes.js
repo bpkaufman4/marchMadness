66709c01cf9a47cec540efde442592a647362c40
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-12 16:39:27 -0500

    'BK'

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { User } = require('../models');
 
 router.get('/adminUsers', (req, res) => {
     res.render('users', {layout: 'admin'});
@@ -7,4 +8,24 @@ router.get('/adminUsers', (req, res) => {
 router.get('/userUsers', (req, res) => {
     res.render('users');
 });
+
+router.get('/adminUser/:id', (req, res) => {
+    User.findOne({
+        where: {
+            userId: req.params.userId
+        }
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+            return;
+        }
+        const user = dbUserData.get({ plain: true });
+        res.render('user', { user, layout: 'admin' });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+})
 module.exports = router;
