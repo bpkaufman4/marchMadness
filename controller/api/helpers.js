2007661d4b6733044ecc5f392ea7afe6cb575a0c
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-12 16:09:53 -0500

    'BK'

--- controller/api/helpers.js
+++ controller/api/helpers.js
@@ -2,17 +2,22 @@
 const { User } = require('../../models');
 
 const getUnusedUserPerma = function(seed) {
+    console.log(seed);
+    var newSeed;
+
     User.findOne({
         where: {
             perma: seed
         }
     }).then(userData => {
+        console.log(userData);
         if(userData) {
-            return getUnusedUserPerma(seed+'1');
+            newSeed = getUnusedUserPerma(seed+'1');
         } else {
-            return seed;
+            newSeed = seed;
         }
     });
+    return newSeed;
 }
 
 module.exports = { getUnusedUserPerma };
