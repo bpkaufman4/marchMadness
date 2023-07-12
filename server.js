3e2a3009a424639bfd65cd7eec659e4e82429268
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-12 16:43:36 -0500

    'BK'

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.engine('handlebars', hbs.engine);
 
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(controller);
