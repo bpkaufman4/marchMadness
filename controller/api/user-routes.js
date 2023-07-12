4e122e2541c81b9d06494ada4297bfa7e2555b04
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-12 12:15:31 -0500

    'BK'

--- controller/api/user-routes.js
+++ controller/api/user-routes.js
@@ -55,6 +55,17 @@ router.get('/:userId', (req, res) => {
     });
 });
 
+router.get('/', (req, res) => {
+    User.findAll()
+    .then(dbUserData => {
+        res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
 router.post('/', (req, res) => {
     User.create({
         email: req.body.email,
