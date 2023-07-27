7044df9df222f8e87d3b2ee1554f05cff6a9c32f
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-27 13:45:58 -0500

    force db

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(controller);
-sequelize.sync({ alter: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => {
         console.log(`listening on port ${PORT}`);
     });
