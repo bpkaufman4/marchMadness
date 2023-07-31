1742ed19d78b1ca9915def750a6cad86c8b0954b
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-31 16:07:51 -0500

    'BK'

--- controller/modelDocs/index.js
+++ controller/modelDocs/index.js
@@ -4,10 +4,12 @@ const userModelDoc = require('./userModelDocs');
 const referenceModelDoc = require('./referenceModelDocs');
 const referenceSetsModelDoc = require('./referenceSetsModelDocs');
 const staticContentModelDoc = require('./staticContentModelDocs');
+const changeLogModelDoc = require('./changeLogModelDocs');
 
 router.use('/user', userModelDoc);
 router.use('/reference', referenceModelDoc);
 router.use('/staticContent', staticContentModelDoc);
 router.use('/referenceSets', referenceSetsModelDoc);
+router.use('/changeLog', changeLogModelDoc);
 
 module.exports = router;
