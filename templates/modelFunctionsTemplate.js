7607b1d86f637b3fc1a5a7428ac14ae43104ee33
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-31 15:22:15 -0500

    'BK'

--- templates/modelFunctionsTemplate.js
+++ templates/modelFunctionsTemplate.js
@@ -184,6 +184,21 @@ module.exports = { get${snakeCase}Function, delete${snakeCase}Function, put${sna
 
 function generateModelDoc(request) {
     console.log(request);
+    const template = `const router = require('express').Router();
+
+    router.get('/get', (req, res) => {
+        const requestFields = ['email', 'userId', 'emailVerifyGUID'];
+        const endpoint = 'users/getUser';
+        const renderData = {requestFields, endpoint};
+    
+        res.render('modelFileExercisor', renderData);
+    });
+    
+    router.get('/put', (req, res) => {
+        const requestFields = []
+    })
+    module.exports = router;
+    `;
 }
 
 module.exports = createModelFunctionsFile;
