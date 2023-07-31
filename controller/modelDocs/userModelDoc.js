93d436cec4dddac89823e7a9966618bf3acf3115
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-31 13:15:43 -0500

    'BK'

--- controller/modelDocs/userModelDoc.js
+++ controller/modelDocs/userModelDoc.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/get', (req, res) => {
     const requestFields = ['email', 'userId', 'emailVerifyGUID'];
-    const endpoint = 'user/getUser';
+    const endpoint = 'users/getUser';
     const renderData = {requestFields, endpoint};
 
     res.render('modelFileExercisor', renderData);
