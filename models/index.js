3e5190a7fc692af725ce00b943694a932daba1ed
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-31 16:03:32 -0500

    'BK'

--- models/index.js
+++ models/index.js
@@ -2,8 +2,9 @@ const User = require('../models/User');
 const Reference = require('../models/Reference');
 const ReferenceSet = require('../models/ReferenceSet');
 const StaticContent = require('../models/StaticContent');
+const ChangeLog = require('../models/ChangeLog');
 
 User.belongsTo(Reference, {foreignKey: 'statusCd', as: 'userStatus'});
 User.belongsTo(Reference, {foreignKey: 'userTypeCd', as: 'userType'});
 
-module.exports = { User, Reference, ReferenceSet, StaticContent };
+module.exports = { User, Reference, ReferenceSet, StaticContent, ChangeLog };
