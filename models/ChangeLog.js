cad6203b869d3410b4cd1778f8f9f5f51797db75
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-07-31 16:01:23 -0500

    'BK'

---
+++ models/ChangeLog.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const { User } = require('../models');
+const sequelize = require('../config/connection');
+
+class ChangeLog extends Model {};
+
+ChangeLog.init(
+    {
+        changeLogId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true
+        },
+        changeDetails: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        changeDateTime: {
+            type: DataTypes.DATETIME,
+            allowNull: false,
+            defaultValue: sequelize.literal('NOW()'),
+        },
+        userId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: User,
+                key: 'userId'
+            }
+        },
+        parentId: DataTypes.UUID,
+        parentName: DataTypes.STRING,
+        templateType: DataTypes.STRING
+    },
+    {
+        sequelize,
+        timestamps: true,
+        createdAt: 'created',
+        updatedAt: 'updated',
+        freezeTableName: true,
+        underscored: false,
+        modelName: 'changeLog'
+    }
+);
+
+module.exports = ChangeLog;
