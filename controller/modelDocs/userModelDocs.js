27a2d3305431641487f346949cb7942331c1472c
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-01 16:36:03 -0500

    'BK'

--- controller/modelDocs/userModelDocs.js
+++ controller/modelDocs/userModelDocs.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-    router.get('/get', (req, res) => {
+    router.get('get', (req, res) => {
         const requestFields = ['userId', 'email', 'lastName', 'emailVerifyGUID'];
         const endpoint = 'user/getUser';
         const renderData = {requestFields, endpoint};
@@ -8,7 +8,7 @@ const router = require('express').Router();
         res.render('modelFileExercisor', renderData);
     });
     
-    router.get('/put', (req, res) => {
+    router.get('put', (req, res) => {
         const requestFields = ['userId', 'email', 'pwd', 'lastName', 'firstName', 'statusCd', 'statusCdMeaning', 'userTypeCd', 'userTypeCdMeaning', 'lastLoginDate', 'lastIP', 'primaryPhone', 'cellPhone', 'state', 'zip', 'emailVerifyGUID', 'emailVerifyExpire', 'timeZoneId', 'lastActiveDateTime', 'profilePictureURL', 'profilePictureLocal', 'created', 'updated', 'deletedAt', 'bksTestColumn'];
         const endpoint = 'user/putUser';
         const renderData = {requestFields, endpoint};
@@ -16,7 +16,7 @@ const router = require('express').Router();
         res.render('modelFileExercisor', renderData);
     })
 
-    router.get('/delete', (req, res) => {
+    router.get('delete', (req, res) => {
         const requestFields = ['userId'];
         const endpoint = 'user/deleteUser';
         const renderData = {requestFields, endpoint};
