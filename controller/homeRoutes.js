83d81242a1e1b2cf9eab7483d77fbc6e74295e83
JackKuckelman <jackattack1324@gmail.com>
2023-08-03 13:54:39 -0500

    'JACK'

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../models');
 
 router.get('/adminUsers', (req, res) => {
-    res.render('users', {layout: 'admin', users: User.findAll});
+    res.render('users', {layout: 'admin', users: User.findAll()});
 });
 
 router.get('/userUsers', (req, res) => {
