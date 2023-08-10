1e727ff7bb4b5819243b800ed723867862ebf113
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-10 12:40:42 -0500

    'BK'

--- controller/api/user-routes/index.js
+++ controller/api/user-routes/index.js
@@ -1,12 +1,5 @@
 const router = require('express').Router();
 
-/*
-------------------------------Paste into api/index.js----------------------------------
-const userRoutes = require('./user-routes');
-router.use('/user', userRoutes);
----------------------------------------------------------------------------------------
-*/
-
 const getRoutes = require('./get');
 const putRoutes = require('./put');
 const deleteRoutes = require('./delete');
