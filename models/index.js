9812cf9865a6c69d2c18eff4574863e73b055f73
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-10 15:30:59 -0500

    'BK'

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const Post = require('../models/Post');
 
 User.belongsTo(Reference, {foreignKey: 'statusCd', as: 'userStatus'});
 User.belongsTo(Reference, {foreignKey: 'userTypeCd', as: 'userType'});
+
 Post.belongsTo(User, {foreignKey: 'userId', as: 'user'});
 User.hasMany(Post, {foreignKey: 'userId', as: 'posts'});
 
