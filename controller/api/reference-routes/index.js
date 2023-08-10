dc4a455468a10e6a2183029fcebb4ec0a5d4838e
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-10 12:40:09 -0500

    'BK'

--- controller/api/reference-routes/index.js
+++ controller/api/reference-routes/index.js
@@ -1,12 +1,5 @@
 const router = require('express').Router();
 
-/*
-------------------------------Paste into api/index.js----------------------------------
-const referenceRoutes = require('./reference-routes');
-router.use('/reference', referenceRoutes);
----------------------------------------------------------------------------------------
-*/
-
 const getRoutes = require('./get');
 const putRoutes = require('./put');
 const deleteRoutes = require('./delete');
