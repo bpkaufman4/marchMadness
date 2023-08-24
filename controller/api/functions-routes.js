d029430c643dbc5c1b2da06e25c8d1f8b1d3813a
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-24 15:22:16 -0500

    'BK'

--- controller/api/functions-routes.js
+++ controller/api/functions-routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const {generateFunctionsFile} = require('../../templates/modelFunctionsTemplate');
+const {createModelFile} = require('../../templates/modelFunctionsTemplate');
 
 router.post('/generateFunctionsFile', (req, res) => {
     const request = req.body;
-    generateFunctionsFile(request);
+    createModelFile(request);
     res.json({message: 'Success'});
 });
 
