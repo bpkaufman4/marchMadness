2935c10b1631fe7762a30c2975e602c1556ca6d1
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-24 15:26:44 -0500

    'BK'

--- controller/api/functions-routes.js
+++ controller/api/functions-routes.js
@@ -3,9 +3,10 @@ const {createModelFile} = require('../../templates/modelFunctionsTemplate');
 
 router.post('/generateFunctionsFile', (req, res) => {
     const request = req.body;
-    const createModel = createModelFile(request);
-    console.log(createModel);
-    res.json({status: 'Success', message: createModel.message});
+    createModelFile(request).then(response => {
+        console.log(response);
+        res.json({status: 'Success', message: createModel.message});
+    });
 });
 
 module.exports = router;
