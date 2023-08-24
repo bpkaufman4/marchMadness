9e83c68143bb424af964b10dba0d8f262bf08565
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-24 16:57:57 -0500

    'BK'

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     console.log(req.socket.remoteAddress);
-    putIpLoggingFunction({remote_addr: req.socket.remoteAddress, request_uri: req.url});
+    putIpLoggingFunction({remote_addr: req.socket.remoteAddress, request_uri: req.url, http_x_real_ip: req.headers['x-real-ip'], request: req.body,});
     next();
 });
 app.use(controller);
