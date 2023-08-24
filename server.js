23880698941869c288f889420cefcd244b37747d
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-24 16:38:42 -0500

    'BK'

--- server.js
+++ server.js
@@ -32,6 +32,9 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(controller);
 
+app.use((req, res) => {
+    console.log(req);
+});
 
 sequelize.sync({ alter: false }).then(() => {
     app.listen(PORT, () => {
