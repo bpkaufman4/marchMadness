b0d89137403271b5822708cedbc3383f2131289f
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-24 15:12:53 -0500

    'BK'

--- controller/api/functions-routes.js
+++ controller/api/functions-routes.js
@@ -4,6 +4,7 @@ const generateFunctionsFile = require('../../templates/modelFunctionsTemplate');
 router.post('/generateFunctionsFile', (req, res) => {
     const request = req.body;
     var generateFunctions = generateFunctionsFile(request);
+    console.log(generateFunctions);
     res.json({status: 'Success', message: generateFunctions.message});
 });
 
