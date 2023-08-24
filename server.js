1dc72f354ec4a145aef61730eed24899d0f329d8
bpkaufman4 <131032856+briankprogrammatis@users.noreply.github.com>
2023-08-24 16:49:21 -0500

    'BK'

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
-    console.log(req.socket);
+    console.log(req.socket.remoteAddress);
     putIpLoggingFunction({remote_addr: req.socket.remoteAddress});
     next();
 });
