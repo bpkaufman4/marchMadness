eff4c62236b0d3f225e90db3e7e4a4e380186f02
briankprogrammatis <131032856+briankprogrammatis@users.noreply.github.com>
2023-10-06 13:48:55 -0500

    'BK'

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use(controller);
 
 
-sequelize.sync({ alter: true }).then(() => {
+sequelize.sync({ alter: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`listening on port ${PORT}`);
     });
