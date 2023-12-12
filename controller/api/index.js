57105739699927b0b1c5c0c1f8b160adba54f944
Brian Kaufman <bpkaufman34@gmail.com>
2023-12-12 16:05:06 -0600

    'BK'

--- controller/api/index.js
+++ controller/api/index.js
@@ -5,14 +5,10 @@ const routesRoutes = require('./routes-routes');
 const functionsRoutes = require('./functions-routes');
 const referenceRoutes = require('./reference-routes');
 const referenceSetsRoutes = require('./referenceSets-routes');
-const changeLogRoutes = require('./changeLog-routes');
-const postRoutes = require('./post-routes');
 
 router.use('/user', userRoutes);
 router.use('/routes', routesRoutes);
 router.use('/functions', functionsRoutes);
 router.use('/reference', referenceRoutes);
 router.use('/referenceSets', referenceSetsRoutes);
-router.use('/changeLog', changeLogRoutes);
-router.use('/post', postRoutes);
 module.exports = router; 
