824965038975a7b17d0647b316fe3c3c27ada6d3
Brian Kaufman <briank@programmatis.com>
2024-02-18 15:45:48 -0600

    'BK'

--- controller/api/Event-routes/index.js
+++ controller/api/Event-routes/index.js
@@ -8,9 +8,11 @@ const router = require('express').Router();
 const getRoutes = require('./get');
 const putRoutes = require('./put');
 const deleteRoutes = require('./delete');
+const bultCreateRoutes = require('./bultCreate');
 
 router.use('/get', getRoutes);
 router.use('/put', putRoutes);
 router.use('/delete', deleteRoutes);
+router.use('/bulkCreate', deleteRoutes);
 
 module.exports = router; 
