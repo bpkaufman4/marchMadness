96ce2655713fd08c3b633f12963f8dd8798130d8
Brian Kaufman <briank@programmatis.com>
2024-02-18 17:14:39 -0600

    'BK'

--- public/javascript/sportsdata-helpers.js
+++ public/javascript/sportsdata-helpers.js
@@ -60,8 +60,7 @@ async function syncTeamsFromEvents(event) {
 
 async function syncPlayersFromTeams(e) {
     const elem = e.target;
-    const team = elem.dataset;
-    const url = `https://api.sportsdata.io/v3/cbb/scores/json/PlayersBasic/${team.apiId}?key=e20de04c19364639908688eda889dea1`;
+    const url = `https://api.sportsdata.io/v3/cbb/scores/json/PlayersBasic/${elem.dataset.apiId}?key=e20de04c19364639908688eda889dea1`;
     const options = {
         method: 'GET'
     };
