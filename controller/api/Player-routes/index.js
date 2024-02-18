df80818bbca6c3ff9e0978beb12a5fa62d31ec89
Brian Kaufman <briank@programmatis.com>
2024-02-18 17:27:47 -0600

    'BK'

--- controller/api/Player-routes/index.js
+++ controller/api/Player-routes/index.js
@@ -8,9 +8,11 @@ const router = require('express').Router();
 const getRoutes = require('./get');
 const putRoutes = require('./put');
 const deleteRoutes = require('./delete');
+const bulkCreateRoutes = require('./bulkCreate');
 
 router.use('/get', getRoutes);
 router.use('/put', putRoutes);
 router.use('/delete', deleteRoutes);
+router.use('/bulkCreate', bulkCreateRoutes);
 
 module.exports = router; 
