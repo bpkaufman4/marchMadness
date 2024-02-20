3683df0eed08f31e85f640141877f2cf66188e81
Brian Kaufman <briank@programmatis.com>
2024-02-20 17:17:41 -0600

    'BK'

--- cron.js
+++ cron.js
@@ -1,7 +1,15 @@
 const cron = require('cron-node');
+const https = require('https');
+require('dotenv').config();
 
 function setupCron() {
     pullEvents();
+}
+
+function pullEvents() {
+    https.get(`https://api.sportsdata.io/v3/cbb/scores/json/SchedulesBasic/2023POST?key=${process.env.API_KEY}`, reply => {
+        console.log(reply);
+    });
 }
 
 module.exports = setupCron;
