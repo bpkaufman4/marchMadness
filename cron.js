f061b89a5dfea630b36a6b3b16a8d485feddd0be
Brian Kaufman <briank@programmatis.com>
2024-02-20 17:27:34 -0600

    'BK'

--- cron.js
+++ cron.js
@@ -8,12 +8,7 @@ function setupCron() {
 
 function pullEvents() {
     https.get(`https://api.sportsdata.io/v3/cbb/scores/json/SchedulesBasic/2023POST?key=${process.env.API_KEY}`, (resp) => {
-        let data = '';
-
-        // A chunk of data has been received.
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
+        console.log(resp.body);
 
     }).on("error", (err) => {
         console.log("Error: " + err.message);
