553626660320fea9a16101e41340432c6d55822a
Brian Kaufman <briank@programmatis.com>
2024-02-20 17:22:53 -0600

    'BK'

--- cron.js
+++ cron.js
@@ -8,9 +8,23 @@ function setupCron() {
 
 function pullEvents() {
     https.get(`https://api.sportsdata.io/v3/cbb/scores/json/SchedulesBasic/2023POST?key=${process.env.API_KEY}`, reply => {
-        reply.on('end', data => {
-            console.log(data);
+        
+    });
+    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+        let data = '';
+
+        // A chunk of data has been received.
+        resp.on('data', (chunk) => {
+            data += chunk;
         });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(JSON.parse(data).explanation);
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
     });
 }
 
