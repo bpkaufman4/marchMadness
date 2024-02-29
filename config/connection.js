1cb48342a58c228197b3437314c3e45a12d6a795
Brian Kaufman <briank@programmatis.com>
2024-02-29 10:51:57 -0600

    changed db host

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,15 @@ require('dotenv').config();
 
 let sequelize;
 
-if(process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306,
-        logging: true,
-        dialectOptions: {
-            multipleStatements: true
-        }
-    });
-}
+sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT,
+    logging: true,
+    dialectOptions: {
+        multipleStatements: true
+    }
+});
 
 
 module.exports = sequelize;
